ca0b8c38912098c156bba75c19171e847596a142
Luan Huanca <luanhuancam@gmail.com>
2024-02-21 23:42:10 -0400

    funcion agregar tarea

--- ToDoList.py
+++ ToDoList.py
@@ -1,3 +1,24 @@
+from Tarea import Tarea
+
 class TodoList:
     def __init__(self):
         self.tareas = []
+
+    def agregar_tarea(self):
+        while True:
+            descripcion = input("Ingrese la descripción de la nueva tarea: ")
+            if descripcion:
+                break
+            else:
+                print("La descripción no puede estar vacía. Inténtelo de nuevo.")
+
+        while True:
+            prioridad = input("Ingrese la prioridad de la tarea (baja, media, alta): ")
+            if prioridad.lower() in ["baja", "media", "alta"]:
+                break
+            else:
+                print("Prioridad inválida. Por favor, elija entre 'baja', 'media' o 'alta'.")
+
+        estado = "sin iniciar"
+        self.tareas.append(Tarea(descripcion, estado, prioridad))
+        print("Tarea agregada exitosamente.")
